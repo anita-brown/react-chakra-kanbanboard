a60baa2231a464a621cf607419fa2b13cdd5594c
anita-brown <efeaniita@gmail.com>
2023-01-05 00:17:53 +0100

    chore: created enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Container, Heading } from '@chakra-ui/react'
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Heading fontSize={{base: '4xl', sm: '5xl', md: '6xl'}} fontWeight="bold" textAlign="center" bgGradient="linear(to-l, #7928CA, #FF0080" bgClip="text" ml={4}>
+        Welcome to Dnd Kanban
+      </Heading>
+      <Container maxWidth="container.lg" px={4} py={10}>
+
+      </Container>
+    </>
   )
 }
 
