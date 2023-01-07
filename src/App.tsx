eb9280b98357e6083383fc5cd4cfda1cd545da42
anita-brown <efeaniita@gmail.com>
2023-01-08 00:11:17 +0100

    chore: added darkmodeiconbutton

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,30 @@ import { useState } from "react";
 import Column from "./components/Column";
 import Task from "./components/Task";
 import { ColumnType } from "./utils/enum";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const handleAddTask = (column: ColumnType) => { 
-    console.log("Add task to column", column)
-  }
-
+  
+  
   return (
-    <>
-      <Heading fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }} fontWeight="bold" textAlign="center" bgGradient="linear(to-l, #7928CA, #FF0080" bgClip="text" ml={4}>
-        Welcome to Dnd Kanban
+    <main>
+      <Heading fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }} fontWeight="bold" textAlign="center" bgGradient="linear(to-l, #7928CA, #FF0080)" bgClip="text" mt={4}>
+        Welcome to DnD Kanban
       </Heading>
-      <Container maxWidth="container.lg" px={4} py={10}>
-        <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
-          <Column column={ColumnType.TO_DO} />
-          <Column column={ColumnType.IN_PROGRESS} />
-          <Column column={ColumnType.BLOCKED} />
-          <Column column={ColumnType.COMPLETED} />
-        </SimpleGrid>
-      </Container>
-    </>
+      <DarkModeIconButton position="absolute" top={0} right={2} />
+      <DndProvider backend={HTML5Backend}>
+        <Container maxWidth="container.lg" px={4} py={10}>
+          <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
+            <Column column={ColumnType.TO_DO} />
+            <Column column={ColumnType.IN_PROGRESS} />
+            <Column column={ColumnType.BLOCKED} />
+            <Column column={ColumnType.COMPLETED} />
+          </SimpleGrid>
+        </Container>
+      </DndProvider>
+    </main>
   );
 }
 
